320d71e8cdcc3b800fa51460ca82a2f4de7e6ba8
Yohei Kakiuchi <kakiuchi.yohei.sw@tut.jp>
2023-11-30 15:16:25 +0900

    update build.sh

--- docker/build.sh
+++ docker/build.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-docker build .. --pull -f Dockerfile -t irslrepo/irsl_hr4c:noetic
+BASE_IMG=irslrepo/irsl_system:noetic
+
+docker build .. --progress=plain --pull -f Dockerfile \
+       --build-arg BASE_IMAGE=${BASE_IMG} -t irslrepo/irsl_hr4c:noetic
+
+## docker login
+## docker push irslrepo/irsl_hr4c:noetic
