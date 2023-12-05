eb8696d5c86e4b7ed3a9a7eca4f5600a1d80c7cb
Yohei Kakiuchi <kakiuchi.yohei.sw@tut.jp>
2023-12-05 22:19:44 +0900

    add methods for gripper to KohakuInterface

--- models/kohaku_robotinterface.py
+++ models/kohaku_robotinterface.py
@@ -4,12 +4,31 @@
 
 from irsl_choreonoid_ros import RobotInterface
 
-class KohakuInterface(RobotInterface):
+class KohakuInterface(RobotInterface.RobotInterface):
     def __init__(self, file_name, **kwargs):
-        RobotInterface.__init__(self, file_name, **kwargs)
+        RobotInterface.RobotInterface.__init__(self, file_name, **kwargs)
 
-    def graps(self, hand):
-        pass
+        self.is_real_robot = self.isRealRobot()
 
-    def ungraps(self, hand):
-        pass
+        if self.is_real_robot:
+            self.right_gripper_action = RobotInterface.GripperInterface('/kohaku_righthand/gripper_cmd')
+            self.left_gripper_action  = RobotInterface.GripperInterface('/kohaku_lefthand/gripper_cmd')
+
+    def grasp(self, hand, position, effort=10):
+        hh = hand.lower()
+        if hh == 'l' or hh == 'left':
+            hand = 'left'
+        else:
+            hand = 'right'
+
+        if self.is_real_robot:
+            pass
+        else:
+            jg = self.getJointGroup('{}hand'.format(hand))
+            jg.sendAnglesSequence(((position, position), ), (effort/10.0, ))
+
+    def ungrasp(self, hand, effort=10):
+        if self.is_real_robot:
+            pass
+        else:
+            self.grasp(hand, 1.5708, effort)
