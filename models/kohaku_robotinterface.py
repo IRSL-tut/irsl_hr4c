6f490b7e530341d795810812a367ce4da036e783
Yohei Kakiuchi <kakiuchi.yohei.sw@tut.jp>
2023-12-18 23:23:49 +0900

    update kohaku_robotinterface.py

--- models/kohaku_robotinterface.py
+++ models/kohaku_robotinterface.py
@@ -18,9 +18,9 @@ class KohakuInterface(RobotInterface.RobotInterface):
             self.right_gripper_action = RobotInterface.GripperInterface('/kohaku_righthand/gripper_cmd')
             self.left_gripper_action  = RobotInterface.GripperInterface('/kohaku_lefthand/gripper_cmd')
 
-    def grasp(self, hand, position, effort=10):
+    def grasp(self, hand, position, effort=10, wait=False, waitTimeout=15.0):
         hh = hand.lower()
-        if hh == 'l' or hh == 'left':
+        if hh in ( 'l', 'left', 'larm', 'L', 'Left', 'Larm'):
             hand = 'left'
         else:
             hand = 'right'
@@ -30,9 +30,13 @@ class KohakuInterface(RobotInterface.RobotInterface):
         else:
             jg = self.getJointGroup('{}hand'.format(hand))
             jg.sendAnglesSequence(((position, position), ), (effort/10.0, ))
+            if wait:
+                jg.waitUntilFinish(waitTimeout)
 
-    def ungrasp(self, hand, effort=10):
+    def ungrasp(self, hand, effort=10, wait=False, waitTimeout=15.0):
         if self.is_real_robot:
             pass
         else:
             self.grasp(hand, 1.5708, effort)
+            if wait:
+                jg.waitUntilFinish(waitTimeout)
